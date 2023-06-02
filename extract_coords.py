554dd269a8b01f4b31f1dc21a3432f6cc946990e
Jacob Moore <billingsmoore@gmail.com>
2023-06-02 11:27:23 -0400

    allowed user to choose pdf

--- extract_coords.py
+++ extract_coords.py
@@ -2,7 +2,9 @@ from pypdf import PdfReader
 import re
 import csv
 
-reader = PdfReader("test.pdf")
+pdf = input('Enter name of pdf: ')
+
+reader = PdfReader(pdf)
 text = ""
 for page in reader.pages:
     text += page.extract_text() + "\n"
@@ -12,8 +14,6 @@ pattern = r"\d+\.\d+/\s*\d+\.\d+"
 text = str(text).replace('\00', '')
 
 matches = re.findall(pattern, text)
-
-print(matches[0])
 
 with open("Output.csv", "w") as f:
     writer = csv.writer(f)
