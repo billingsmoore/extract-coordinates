0c4c67959bbfcfffd402b5187fa4d6ccefef60f1
Jacob Moore <billingsmoore@gmail.com>
2023-06-02 16:04:36 -0400

    annotated code for user-friendliness

--- extract_coords.py
+++ extract_coords.py
@@ -2,25 +2,34 @@ from pypdf import PdfReader
 import re
 import csv
 
+# get name of pdf from user
 pdf = input('Enter name of pdf: ')
 
+# initialize a pdf reader
 reader = PdfReader(pdf)
+
+# read the text off of the pdf
 text = ""
 for page in reader.pages:
     text += page.extract_text() + "\n"
 
+# regex to get the coordinates from text
 pattern = r"\d+\.\d+/\s*\d+\.\d+"
 
+# strip out the null values when the pdf reader mangles ukrainian
+# nulls mess up the pattern matching
 text = str(text).replace('\00', '')
 
 matches = re.findall(pattern, text)
 
+# turn the coordinates into a csv friendly syntax
 for i in range(len(matches)):
     matches[i] = matches[i].split('/ ')
 
-
+# write the coordinates out to csv
 with open("Output.csv", "w") as f:
     writer = csv.writer(f)
+    # put header in the csv
     f.write('latitude, longitude\n')
     for match in matches:
         writer.writerow(match)
